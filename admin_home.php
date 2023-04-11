f6b8c1187c73f13660269e21c40f93bb12e8ebf7
smooth-glitch <66371034+smooth-glitch@users.noreply.github.com>
2023-04-11 22:33:30 +0100

    Create admin_home.php

---
+++ admin_home.php
@@ -0,0 +1,33 @@
+<?php
+// establish database connection
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// retrieve user data from database
+$sql = "SELECT * FROM employee_info";
+$result = mysqli_query($conn, $sql);
+
+// display user data in table
+if (mysqli_num_rows($result) > 0) {
+    echo "<table>";
+    echo "<tr><th>Name</th><th>Email</th><th>Phone</th></tr>";
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<tr><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td><td>" . $row["phone"] . "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+// close database connection
+mysqli_close($conn);
+?>
