36bebf34206a3a7ea68cae0de1f70e300f644d10
humzamughal <64078241+humzamughal@users.noreply.github.com>
2023-04-11 21:18:23 +0100

    Delete announcement.sql

--- announcement.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE announcements;
-
-
-CREATE TABLE employee_info (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    announcement_name VARCHAR(255) NOT NULL UNIQUE,
-    announcement_body VARCHAR(1000) NOT NULL UNIQUE,
-    anouncement_date VARCHAR(20) NOT NULL,
-);
