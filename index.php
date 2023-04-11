08d2574aa1f68113fb5989e76c346ec7c1850a2f
smooth-glitch <66371034+smooth-glitch@users.noreply.github.com>
2023-04-11 22:48:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
 	<title>login-system</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-	<a href="admin_home.php">View User Table</a>
+	
 	
 </head>
 <body>
