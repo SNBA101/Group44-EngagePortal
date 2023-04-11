089a3c5c342f235bbd2d04de47c93adba8bc7d46
smooth-glitch <66371034+smooth-glitch@users.noreply.github.com>
2023-04-11 22:51:33 +0100

    Update home.php

--- home.php
+++ home.php
@@ -7,6 +7,7 @@
 <html>
 <head>
 	<title>HOME</title>
+	<a href="admin_home.php">View User Table</a>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
@@ -14,6 +15,7 @@
       style="min-height: 100vh">
       	<?php if ($_SESSION['role'] == 'admin') {?>
       		<!-- For Admin -->
+	      	<a href="admin_home.php">View User Table</a>
       		<div class="card" style="width: 18rem;">
 			  <img src="img/admin.png" 
 			       class="card-img-top" 
