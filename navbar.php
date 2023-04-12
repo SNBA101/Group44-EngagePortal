8cfef6e2bc3cd2c5e18ef4482a55092275f000c3
Samuel Acheampong <s.acheampong@se21.qmul.ac.uk>
2023-04-12 21:10:04 +0100

    1

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Your Website Title</title>
+  <link rel="stylesheet" type="text/css" href="styleNav.css">
+</head>
+<body>
+
+<nav class="navbar">
+  <a href="home.php" class="navbar-brand">Engage Portal</a>
+  <ul class="navbar-nav">
+  <li class="nav-item"><a href="home.php" class="nav-link">Home</a></li>
+    <li class="nav-item"><a href="announcements2.php" class="nav-link">Announcements</a></li>
+    <li class="nav-item"><a href="my_info.php" class="nav-link">Contact Information</a></li>
+    <li class="nav-item"><a href="training_material.php" class="nav-link">Training Material</a></li>
+    <li class="nav-item"><a href="payslips.php" class="nav-link">Payslips</a></li>
+</nav>
+
+</body>
+</html>
